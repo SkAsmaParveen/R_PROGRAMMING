ac01fca21411cc8d6dc63d5f70a2fc7f2fff5f81
SHAIK ASMA PARVEEN <112798058+SkAsmaParveen@users.noreply.github.com>
2023-01-31 09:33:22 +0530

    Update ConTable.r

--- ConTable.r
+++ ConTable.r
@@ -1,6 +1,11 @@
 # Goodness of fit contingency table
 m<-as.table(rbind(c(190,243,197),c(82,44,44),c(23,78,34),c(5,12,8)))
 dimnames(m)=list(Empcategory=c("Labour","Clerks","Technicians","Executives"), BonusSchemes=c("Type1","Type2","Type3"))
+
+# Unlike rownames() or colnames() the dimnames() command operates on both rows
+# and columns at once. If you use it to set the names you need
+# to specify the names for the rows and columns in that order) in a list.
+
 print(m)
 csum<-colSums (m)
 rsum<-rowSums (m)
